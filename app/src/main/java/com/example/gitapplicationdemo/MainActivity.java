85a5c87605049348b811a9cb298234ac6a4535c4
Projet <badismoalla@gmail.com>
2021-09-28 15:06:28 +0100

    add log to MainActivity

--- app/src/main/java/com/example/gitapplicationdemo/MainActivity.java
+++ app/src/main/java/com/example/gitapplicationdemo/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.gitapplicationdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,MSG);
         setContentView(R.layout.activity_main);
     }
+
+
 }
